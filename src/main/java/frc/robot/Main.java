b62b5cda2e1f97cc79cd8c6dc1038daf173d4a0a
Theodore Mui <69700276+theodoremui@users.noreply.github.com>
2023-09-08 00:15:02 +0000

    adding my name

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -11,6 +11,6 @@ public final class Main {
 
   public static void main(String... args) {
     RobotBase.startRobot(Robot::new);
-    System.out.println("Hello world");
+    System.out.println("Theodore");
   }
 }
